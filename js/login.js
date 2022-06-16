f1109c9d1680e572db733d0bff2200d84cf5f2d2
nicotiza <nico.tiza@gmail.com>
2022-06-15 22:06:45 -0300

    JS: Login + Register V2.0 Pulish: Footer + Cards

--- js/login.js
+++ js/login.js
@@ -1,36 +1,51 @@
-(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = [...document.getElementsByClassName('needs-validation')];
-    const email = document.getElementById('emailUsuario');
-    const password = document.getElementById('contraseñaUsuario');
-    const rememberMe = document.getElementById('recuerdameCheck');
-    // Loop over them and prevent submission
+const formulario = document.getElementById("formulario");
+const usuario = document.getElementById("emailUsuario");
+const alertaUsuario = document.getElementById("emailHelp");
+const contra = document.getElementById("contraseñaUsuario");
+const alertaContra = document.getElementById("passHelp");
+const rememberMe = document.getElementById("recuerdameCheck");
 
-    email.value = this.localStorage.getItem('email')
+//Obtener datos válidos del LocalStorage:
+let validUser = JSON.parse(localStorage.getItem("usuario"));
 
-    forms.forEach(form => {
-      form.addEventListener('submit', function(event) {
-        console.log(email.value);
-        console.log(password.value);
-        console.log(rememberMe.value);
-        if (email.value && password.value) {        
-          form.classList.add('was-validated');
 
-          if(rememberMe){
-            localStorage.setItem('email', email.value);
-          }
+//Captura el Submit que realiza el "formulario". Es una función anónima, con 1 parámetro llamado "evento"(puede ser cualquier nombre, pero les gusta usar la "e").
+formulario.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let okUser = false;
+    let okPass = false;
+    //Validar el mail del Usuario:
+    if (usuario.value !== validUser.email) {
+        console.log("Usuario mal escrito.");
+        alert("Mal ingreso del usuario");
+        alertaUsuario.classList.add("text-danger"); //Cambios en Etiqueta Small: usuario incorrecto. Texto Rojo
+        alertaUsuario.classList.add("font-weight-bold"); //Cambios en Etiqueta Small: usuario incorrecto. Texto Negrita
+        alertaUsuario.innerHTML = "Error de usuario, compruebe que esté bien esrcrito."; //Cambios en Etiqueta Small: usuario incorrecto. Cambio de texto.
+        usuario.classList.add("is-invalid");
+    } else {
+        usuario.classList.add("is-valid");
+        okUser = true;
+    }
 
-          if(email.value == 'cristiangadea@gmail.com' && password.value == 'cac22030'){
-            localStorage.setItem('usuario', email.value);
-            window.location = './perfil.html';
-          }
-        }
-
-        event.preventDefault();
-        event.stopPropagation();
-      }, false);
-    });
-  }, false);
-})();
+    //Validar la contraseña del usuario:
+    if (contra.value !== validUser.contraseña) {
+        console.log("No se escribió contraseña.");
+        alert("No ingresaste contraseña.");
+        alertaContra.classList.add("text-danger");
+        alertaContra.classList.add("font-weight-bold");
+        alertaContra.innerHTML = "¡No puedes dejar éste campo vacío!";
+        contra.classList.add("is-invalid");
+    } else {
+        contra.classList.add("is-valid");
+        okPass = true;
+    }
+    
+    if (okUser == true && okPass == true) {
+        swal("Bienvenido! :D", "", "success")
+            .then((value) => {
+                window.location.href = "../index.html";
+            }
+        );
+    }
+    
+})
